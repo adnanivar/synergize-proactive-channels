b421c68c7d565beeff90c54f150adc0d726251c2
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2023-11-22 20:33:32 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,42 +1,27 @@
-def generate_random_data():
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-    main()
 def main():
+
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
     data = generate_random_data()
-
-    return data
-
-
-
-import random    for (let i = 0; i < 10; i++) {
-function generateRandomData() {
-    return data;
-
-}
-    }
-    const data = generateRandomData();
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-        console.log("Random Number: " + item);
-    const data = [];
-function main() {
-    });
-
-main();
-    data.forEach(item => {
-}    for item in data:
 import random
-    return data
-def main():
-    data = generate_random_data()
 
+    return data
+    main()
+
+    for item in data:
 def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")    for item in data:
+def generate_random_data():
 if __name__ == "__main__":
 
-        print(f"Random Number: {item}")
 
     main()
+def main():
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
