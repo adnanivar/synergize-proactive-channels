7e78e3aa976f046ad57d6da9fbce44ca5467c981
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2023-11-22 19:02:17 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,42 @@
+def generate_random_data():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    main()
+def main():
+    data = generate_random_data()
+
+    return data
+
+
+
+import random    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+    return data;
+
+}
+    }
+    const data = generateRandomData();
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+    const data = [];
+function main() {
+    });
+
+main();
+    data.forEach(item => {
+}    for item in data:
+import random
+    return data
+def main():
+    data = generate_random_data()
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+
+    main()
